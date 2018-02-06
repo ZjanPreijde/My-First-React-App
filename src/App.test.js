7d440fd559d4956e85fc8d2bd4c2c0375b3ac798
ZjanPreijde <j.preijde2@upcmail.nl>
2018-02-06 12:07:15 +0100

    My First React App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
+// ./src/App.test.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
+import Title from './components/Title'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('<App />', () => {
+  const app   = shallow(<App />)
+  const title = <Title content="All Recipes" />
+
+  it('wraps everything in a div tag', () => {
+      expect(app).toHaveTagName('div')
+  })
+
+  it('contains a Title', () => {
+    expect(app).toContainReact(title)
+  })
+})
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
